b56dbd0f3db5fcd460316aa6a574ab8b4220b120
yansigit <44089734+yansigit@users.noreply.github.com>
2020-07-19 21:41:59 +0900

    init

---
+++ android/app/src/main/java/com/barrier_free_kiosk_manager_app/MainActivity.java
@@ -0,0 +1,15 @@
+package com.barrier_free_kiosk_manager_app;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "barrier_free_kiosk_manager_app";
+  }
+}
