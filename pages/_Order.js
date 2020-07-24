2ce219892371da1a6f84bb7e927f84c88ff1ea96
yansigit <44089734+yansigit@users.noreply.github.com>
2020-07-24 15:26:07 +0900

    주문 관련 json 파싱, 뷰 구현

--- pages/_Order.js
+++ pages/_Order.js
@@ -2,26 +2,46 @@ import React, {Component} from 'react';
 import {Content} from 'native-base';
 import OrderList from '../components/OrderList';
 
+const json = {
+  Action: 'OrderSubmit',
+  OrderID: 4003,
+  Date: '2020/07/24 2:52PM',
+  OrderList: {
+    0: {
+      Menu: '아메리카노',
+      Count: 3,
+      Options: ['휘핑크림: 추가', '샷: 3', '시럽: 0'],
+    },
+    1: {
+      Menu: '라떼',
+      Count: 2,
+      Options: ['휘핑크림: 없음', '샷: 1', '시럽: 1'],
+    },
+    2: {Menu: '쿠키', Count: 5, Options: ['초코칩']},
+  },
+};
+
 export default class _Order extends Component {
   render() {
     return (
       <Content style={{backgroundColor: 'white'}}>
-        <OrderList
-          thumbnail={require('../images/coffee.jpg')}
-          name="아메리카노"
-          options={['휘핑크림: 추가', '샷: 3', '시럽: 없음']}
-        />
-        <OrderList
-          thumbnail={require('../images/coffee.jpg')}
-          name="아메리카노"
-          options={['휘핑크림: 추가', '샷: 3', '시럽: 없음']}
-        />
-        <OrderList
-          thumbnail={require('../images/coffee.jpg')}
-          name="아메리카노"
-          options={['휘핑크림: 추가', '샷: 3', '시럽: 없음']}
-        />
+        <OrderListCreator />
       </Content>
     );
   }
+}
+
+function OrderListCreator() {
+  let rows = [];
+  for (let i = 0; i < Object.keys(json.OrderList).length; i++) {
+    rows.push(
+      <OrderList
+        key={'OrderList' + i}
+        thumbnail={require('../images/coffee.jpg')}
+        name={json.OrderList[i].Menu + ' X ' + json.OrderList[i].Count}
+        options={json.OrderList[i].Options}
+      />,
+    );
+  }
+  return rows;
 }
