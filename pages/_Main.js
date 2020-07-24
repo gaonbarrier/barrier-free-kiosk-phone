085c15b813516a309d629772042246aaf82d5200
yansigit <44089734+yansigit@users.noreply.github.com>
2020-07-24 16:05:24 +0900

    update

--- pages/_Main.js
+++ pages/_Main.js
@@ -1,11 +1,100 @@
 import React, {Component} from 'react';
 import {Content, Text} from 'native-base';
+import {Dimensions} from 'react-native';
+import {LineChart, PieChart} from 'react-native-chart-kit';
+
+const data = [
+  {
+    name: 'Seoul',
+    population: 21500000,
+    color: 'rgba(131, 167, 234, 1)',
+    legendFontColor: '#7F7F7F',
+    legendFontSize: 15,
+  },
+  {
+    name: 'Toronto',
+    population: 2800000,
+    color: '#F00',
+    legendFontColor: '#7F7F7F',
+    legendFontSize: 15,
+  },
+  {
+    name: 'Beijing',
+    population: 527612,
+    color: 'red',
+    legendFontColor: '#7F7F7F',
+    legendFontSize: 15,
+  },
+  {
+    name: 'New York',
+    population: 8538000,
+    color: '#ffffff',
+    legendFontColor: '#7F7F7F',
+    legendFontSize: 15,
+  },
+  {
+    name: 'Moscow',
+    population: 11920000,
+    color: 'rgb(0, 0, 255)',
+    legendFontColor: '#7F7F7F',
+    legendFontSize: 15,
+  },
+];
+const width = Dimensions.get('window').width;
+const chartConfig = {
+  backgroundColor: '#e26a00',
+  backgroundGradientFrom: '#fb8c00',
+  backgroundGradientTo: '#ffa726',
+  decimalPlaces: 2, // optional, defaults to 2dp
+  color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+  labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+  style: {
+    borderRadius: 16,
+  },
+  propsForDots: {
+    r: '6',
+    strokeWidth: '2',
+    stroke: '#ffa726',
+  },
+};
 
 export default class _Main extends Component {
   render() {
     return (
-      <Content>
-        <Text>메인화면</Text>
+      <Content style={{padding: 10}}>
+        <LineChart
+          data={{
+            labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+            datasets: [
+              {
+                data: [
+                  Math.random() * 100,
+                  Math.random() * 100,
+                  Math.random() * 100,
+                  Math.random() * 100,
+                  Math.random() * 100,
+                  Math.random() * 100,
+                ],
+              },
+            ],
+          }}
+          width={width} // from react-native
+          height={220}
+          yAxisLabel="$"
+          yAxisSuffix="k"
+          yAxisInterval={1} // optional, defaults to 1
+          chartConfig={chartConfig}
+        />
+        <PieChart
+          data={data}
+          width={width}
+          height={220}
+          chartConfig={chartConfig}
+          accessor="population"
+          backgroundColor="transparent"
+          paddingLeft="15"
+          absolute
+        />
       </Content>
     );
   }
--- pages/_Monitoring.js
+++ pages/_Monitoring.js
@@ -1,12 +1,82 @@
 import React, {Component} from 'react';
-import {Content, Text} from 'native-base';
+import {Dimensions, Image} from 'react-native';
+import {
+  Container,
+  Header,
+  Content,
+  Card,
+  CardItem,
+  Thumbnail,
+  Text,
+  Button,
+  Icon,
+  Left,
+  Body,
+  Right,
+} from 'native-base';
+import {ProgressChart} from 'react-native-chart-kit';
+
+const data = {
+  labels: ['Battery', 'Wifi'],
+  data: [0.6, 0.8],
+};
+
+const chartConfig = {
+  backgroundColor: '#e26a00',
+  backgroundGradientFrom: '#fb8c00',
+  backgroundGradientTo: '#ffa726',
+  decimalPlaces: 2, // optional, defaults to 2dp
+  color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+  labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+  style: {
+    borderRadius: 16,
+  },
+  propsForDots: {
+    r: '6',
+    strokeWidth: '2',
+    stroke: '#ffa726',
+  },
+};
+
+const tabletList = {
+  0: {battery: '100', wifi: 'Strong'},
+  1: {battery: '100', wifi: 'Strong'},
+  2: {battery: '100', wifi: 'Strong'},
+};
+
+const width = Dimensions.get('window').width;
 
 export default class _Monitoring extends Component {
   render() {
     return (
-      <Content>
-        <Text>키오스크 모니터링</Text>
+      <Content style={{padding: 10}}>
+        <TabletListCreator />
       </Content>
     );
   }
+}
+
+function TabletListCreator() {
+  let rows = [];
+  for (let i = 0; i < Object.keys(tabletList).length; i++) {
+    rows.push(
+      <Card>
+        <CardItem>
+          <Text>태블릿 {i}</Text>
+        </CardItem>
+        <CardItem cardBody>
+          <ProgressChart
+            data={data}
+            width={width - 15}
+            height={220}
+            strokeWidth={16}
+            radius={32}
+            chartConfig={chartConfig}
+            hideLegend={false}
+          />
+        </CardItem>
+      </Card>,
+    );
+  }
+  return rows;
 }
