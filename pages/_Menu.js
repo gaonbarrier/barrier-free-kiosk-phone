93348979f1761b8dadc3b1c1af909bc5cf88316b
yansigit <44089734+yansigit@users.noreply.github.com>
2020-07-27 17:56:30 +0900

    Update

--- pages/_Menu.js
+++ pages/_Menu.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
-import {Right, Text, ListItem, Button, Body} from 'native-base';
-import {List, Menu} from 'react-native-paper';
+import {Right, Text, ListItem, Button, Body, View} from 'native-base';
+import {StyleSheet} from 'react-native';
+import {List, FAB} from 'react-native-paper';
+import {createStackNavigator} from '@react-navigation/stack';
 
 const MenuList = (props) => (
   <ListItem>
@@ -15,30 +17,58 @@ const MenuList = (props) => (
   </ListItem>
 );
 
-const MyComponent = () => {
+const MenuView = ({ navigation }) => {
   const [expanded, setExpanded] = React.useState(true);
 
   const handlePress = () => setExpanded(!expanded);
 
   return (
-    <List.Section title="메뉴">
-      <List.Accordion
-        title="커피"
-        left={(props) => <List.Icon {...props} icon="equal-box" />}
-        expanded={expanded}
-        onPress={handlePress}>
-        <MenuList menuName="아메리카노" />
-      </List.Accordion>
-      <List.Accordion
-        title="디저트"
-        left={(props) => <List.Icon {...props} icon="equal-box" />}
-        expanded={expanded}
-        onPress={handlePress}>
-        <MenuList menuName="아메리카노" />
-        <MenuList menuName="아메리카노" />
-      </List.Accordion>
-    </List.Section>
+    <View style={{flex: 1}}>
+      <List.Section title="메뉴">
+        <List.Accordion
+          title="커피"
+          left={(props) => <List.Icon {...props} icon="equal-box" />}
+          expanded={expanded}
+          onPress={handlePress}>
+          <MenuList menuName="아메리카노" />
+        </List.Accordion>
+        <List.Accordion
+          title="디저트"
+          left={(props) => <List.Icon {...props} icon="equal-box" />}
+          expanded={expanded}
+          onPress={handlePress}>
+          <MenuList menuName="아메리카노" />
+          <MenuList menuName="아메리카노" />
+        </List.Accordion>
+      </List.Section>
+      <FAB
+        style={styles.fab}
+        icon="plus"
+        onPress={() => navigation.navigate('MenuAdd')}
+      />
+    </View>
   );
 };
 
-export default MyComponent;
+const styles = StyleSheet.create({
+  fab: {
+    position: 'absolute',
+    margin: 16,
+    right: 0,
+    bottom: 0,
+    backgroundColor: '#d9534f',
+  },
+});
+
+const MenuAdd = () => <Text>Test</Text>;
+
+const Stack = createStackNavigator();
+
+const _Menu = () => (
+  <Stack.Navigator>
+    <Stack.Screen name="MenuView" component={MenuView} />
+    <Stack.Screen name="MenuAdd" component={MenuAdd} />
+  </Stack.Navigator>
+);
+
+export default _Menu;
