2ee6dcb173d3abad6407430c56bc1be7549a3135
gem <44089734+yansigit@users.noreply.github.com>
2020-07-31 16:52:50 +0900

    update

--- pages/_Menu.js
+++ pages/_Menu.js
@@ -1,42 +1,10 @@
 import React, {Component} from 'react';
-import {
-  Right,
-  Text,
-  ListItem,
-  Button,
-  Body,
-  View,
-  Content,
-  Item,
-  Input,
-  Label,
-  Thumbnail,
-} from 'native-base';
+import {View, Content, Item, Input, Label} from 'native-base';
 import {StyleSheet, ScrollView} from 'react-native';
-import {List, FAB} from 'react-native-paper';
+import {List, FAB, Menu} from 'react-native-paper';
 import {createStackNavigator} from '@react-navigation/stack';
-import ImagePicker from 'react-native-image-picker';
 
-const options = {
-  title: '이미지 찾기',
-  storageOptions: {
-    skipBackup: true,
-    path: 'images',
-  },
-};
-
-const MenuList = (props) => (
-  <ListItem>
-    <Body style={{marginLeft: 40}}>
-      <Text>{props.menuName}</Text>
-    </Body>
-    <Right>
-      <Button danger>
-        <Text>삭제</Text>
-      </Button>
-    </Right>
-  </ListItem>
-);
+import {MenuItemEditor, MenuList} from '../components/MenuItem';
 
 const MenuView = ({navigation}) => {
   const [expanded, setExpanded] = React.useState(true);
@@ -90,44 +58,25 @@ const MenuAdd = () => (
       <Label>메뉴 설명</Label>
       <Input />
     </Item>
-    <View style={MenuStyle.item}>
-      <ScrollView horizontal={true}>
-        <Thumbnail style={{marginRight:5, backgroundColor: '#dbdbdb'}} square />
-        <Thumbnail style={{marginLeft:5, marginRight:5, backgroundColor: '#dbdbdb'}} square />
-        <Thumbnail style={{marginLeft:5, marginRight:5, backgroundColor: '#dbdbdb'}} square />
-        <Thumbnail style={{marginLeft:5, marginRight:5, backgroundColor: '#dbdbdb'}} square />
-        <Thumbnail style={{marginLeft:5, marginRight:5, backgroundColor: '#dbdbdb'}} square />
-        <Thumbnail style={{marginLeft:5, marginRight:5, backgroundColor: '#dbdbdb'}} square />
-        <Thumbnail style={{marginLeft:5, backgroundColor: '#dbdbdb'}} square />
-      </ScrollView>
-      <View>
-        <Button style={MenuStyle.item_button} onPress={() => getImage()}>
-          <Text>이미지</Text>
-        </Button>
-      </View>
+    <View style={{flexDirection: 'row'}}>
+      <Item stackedLabel style={{flex:1}}>
+        <Label>가격</Label>
+        <Input />
+      </Item>
+      <Item stackedLabel style={{flex:1}}>
+        <Label>가격 2</Label>
+        <Input />
+      </Item>
     </View>
+    <Item stackedLabel style={{alignItems: 'space-between'}}>
+      <Label>재료</Label>
+      <ScrollView horizontal={true} style={{alignSelf: 'stretch'}}>
+        <MenuItemEditor />
+        <MenuItemEditor />
+      </ScrollView>
+    </Item>
   </Content>
 );
-
-const getImage = () =>
-  ImagePicker.launchImageLibrary(options, (response) => {
-    console.log('Response = ', response);
-
-    if (response.didCancel) {
-      console.log('User cancelled image picker');
-    } else if (response.error) {
-      console.log('ImagePicker Error: ', response.error);
-    } else {
-      const source = {uri: response.uri};
-
-      // You can also display the image using data:
-      // const source = { uri: 'data:image/jpeg;base64,' + response.data };
-
-      this.setState({
-        avatarSource: source,
-      });
-    }
-  });
 
 const Stack = createStackNavigator();
 
@@ -140,7 +89,7 @@ const _Menu = () => (
 
 const MenuStyle = StyleSheet.create({
   item: {
-    margin: 10,
+    margin: 5,
   },
   item_image: {
     margin: 10,
