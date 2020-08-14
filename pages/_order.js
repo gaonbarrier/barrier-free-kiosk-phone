401c21df89205332138fa0373ef0a123c89c1a72
gem <44089734+yansigit@users.noreply.github.com>
2020-08-14 15:00:38 +0900

    update

--- pages/_order.js
+++ pages/_order.js
@@ -6,42 +6,50 @@ export default class _order extends Component {
   constructor() {
     super();
     this.state = {
-      Action: 'OrderSubmit',
-      OrderID: 4003,
-      Date: '2020/07/24 2:52PM',
-      OrderList: {
-        0: {
-          Menu: '아메리카노',
-          Count: 3,
-          Options: ['휘핑크림: 추가', '샷: 3', '시럽: 0'],
+      4003: {
+        Status: 'confirmed',
+        Date: '2020/07/24 2:52PM',
+        OrderList: {
+          0: {
+            Menu: '아메리카노',
+            Count: 3,
+            Options: ['휘핑크림: 추가', '샷: 3', '시럽: 0'],
+          },
+          1: {
+            Menu: '라떼',
+            Count: 2,
+            Options: ['휘핑크림: 없음', '샷: 1', '시럽: 1'],
+          },
+          2: {Menu: '쿠키', Count: 5, Options: ['초코칩']},
         },
-        1: {
-          Menu: '라떼',
-          Count: 2,
-          Options: ['휘핑크림: 없음', '샷: 1', '시럽: 1'],
-        },
-        2: {Menu: '쿠키', Count: 5, Options: ['초코칩']},
       },
+      7315: {
+        Date: '2020/07/24 2:52PM',
+        OrderList: {
+          0: {
+            Menu: '아메리카노',
+            Count: 3,
+            Options: ['휘핑크림: 추가', '샷: 3', '시럽: 0'],
+          },
+          1: {
+            Menu: '라떼',
+            Count: 2,
+            Options: ['휘핑크림: 없음', '샷: 1', '시럽: 1'],
+          },
+          2: {Menu: '쿠키', Count: 5, Options: ['초코칩']},
+        },
+      }
     };
   }
 
   render() {
     const OrderListCreator = () => {
       let rows = [];
-      for (let i = 0; i < Object.keys(this.state.OrderList).length; i++) {
-        rows.push(
-          <OrderComponents
-            key={'OrderComponents' + i}
-            thumbnail={require('../images/coffee.jpg')}
-            name={
-              this.state.OrderList[i].Menu +
-              ' X ' +
-              this.state.OrderList[i].Count
-            }
-            options={this.state.OrderList[i].Options}
-          />,
-        );
+
+      for (const [key, value] of Object.entries(this.state)) {
+        console.log(`${key}: ${value}`);
       }
+
       return rows;
     };
     return (
